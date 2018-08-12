890032dab8acd5cc7d06892273056c6d42f0b0d4
jack01234 <1798204267@qq.com>
2018-08-12 18:21:49 +0800

    项目搭建

--- src/main/java/com/wmli/system/controller/HelloController.java
+++ src/main/java/com/wmli/system/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.wmli.system.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Date: 2018/6/17 0017 18:43
  * @Description: test
  */
+@Slf4j
 @Controller
 public class HelloController {
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/hello")
     public String sayHello(ModelMap modelMap){
+        log.info("hello world !!!!");
         modelMap.put("msg", "Hello!");
-        return "hello";
+        return "register";
     }
 }
